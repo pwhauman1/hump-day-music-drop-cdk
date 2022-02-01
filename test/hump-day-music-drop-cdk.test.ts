ec38b4fe8b228019aba690ba99ecc90cb7e25bfb
pwhauman1 <pwhauman@gmail.com>
2022-01-31 19:28:51 -0800

    Initial commit

---
+++ test/hump-day-music-drop-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HumpDayMusicDropCdk from '../lib/hump-day-music-drop-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hump-day-music-drop-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HumpDayMusicDropCdk.HumpDayMusicDropCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
