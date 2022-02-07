a52ffbf57180aac3a010d962a18319c6446b0670
pwhauman1 <pwhauman@gmail.com>
2022-02-06 20:53:49 -0800

    Invoke job lamda every Wednesday

--- src/lib/hump-day-music-drop-cdk-stack.ts
+++ src/lib/hump-day-music-drop-cdk-stack.ts
@@ -5,11 +5,14 @@ import * as ddb from 'aws-cdk-lib/aws-dynamodb';
 import * as path from 'path';
 import { DROPS_TABLE, RECIPIENTS_TABLE, SENDER_JOB_LAMBDA_NAME } from '../constants';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
+import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 
 export class HumpDayMusicDropCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // Table to store recipients
     const recipientsTable = new ddb.Table(this, RECIPIENTS_TABLE, {
       tableName: RECIPIENTS_TABLE,
       partitionKey: {
@@ -19,6 +22,7 @@ export class HumpDayMusicDropCdkStack extends Stack {
       }
     });
 
+    // Table to store the drops that we can pull from
     const dropsTable = new ddb.Table(this, DROPS_TABLE, {
       tableName: DROPS_TABLE,
       partitionKey: {
@@ -31,6 +35,7 @@ export class HumpDayMusicDropCdkStack extends Stack {
       }
     });
 
+    // Lambda that will invoke SES to send an email to everyone subscribed
     const sendJobLambda = new lambda.Function(this, SENDER_JOB_LAMBDA_NAME, {
       runtime: lambda.Runtime.NODEJS_12_X,
       code: lambda.Code.fromAsset(path.join(__dirname, './lambda-project')),
@@ -49,5 +54,14 @@ export class HumpDayMusicDropCdkStack extends Stack {
 
     recipientsTable.grantReadData(sendJobLambda);
     dropsTable.grantReadData(sendJobLambda);
+
+    const scheduleRule = new Rule(this, 'SendJobLambdaScheduler', {
+      schedule: Schedule.cron({
+        weekDay: 'wednesday',
+        hour: '17', // time is in GMT
+        minute: '0',
+      }),
+      targets: [new LambdaFunction(sendJobLambda)]
+    });
   }
 }
